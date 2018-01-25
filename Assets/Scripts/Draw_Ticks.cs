d4e06778e3e7b3fc5970525c94ac46967dc24f23
Voda <mattvodakov@gmail.com>
2018-01-25 12:07:36 -0500

    first load of commit since recovering from attempted Sombra integration

---
+++ Assets/Scripts/Draw_Ticks.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draw_Ticks : MonoBehaviour {
+
+
+    public int numPoints = 10;
+    private List<Vector4> evenCirclePointsList;
+    
+    //public GameObject cylinderGO;
+    public float cylinderRadius;
+    private Vector2 cylinder2DCenter;
+
+    public GameObject tickPrefab;
+
+    // Use this for initialization
+    void Start () {
+        evenCirclePointsList = new List<Vector4>();
+
+        //cylinderRadius = GetComponent<CapsuleCollider>().radius;
+        cylinder2DCenter = new Vector2(transform.position.x, transform.position.z);
+
+        FindCirclePoints(numPoints, cylinderRadius, cylinder2DCenter);
+        //print(numPoints);
+        //print(cylinderRadius);
+        //print(cylinder2DCenter);
+        DrawCirclePoints();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FindCirclePoints(int points, float radius, Vector2 center) {
+        float slice = 2 * Mathf.PI / points;
+        for (int i = 0; i < points; i++) {
+            float angle = slice * i;
+            float newX = (center.x + radius * Mathf.Cos(angle));
+            float newY = (center.y + radius * Mathf.Sin(angle));
+            Vector4 p = new Vector4(newX, newY, transform.position.y, angle);
+            //print(p);
+            //print(angle);
+            evenCirclePointsList.Add(p);
+        }
+    }
+
+
+    void DrawCirclePoints() {
+        for (int i = 0; i < evenCirclePointsList.Count; i++) {
+            GameObject go = Instantiate<GameObject>(tickPrefab, evenCirclePointsList[i], Quaternion.Euler(0f,0f, Mathf.Rad2Deg * evenCirclePointsList[i].w + 90), transform);
+            go.name = "Tick" + i;
+        }
+    }
+}
---
+++ Assets/Scripts/Goal_Detect.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal_Detect : MonoBehaviour {
+
+    public ParticleSystem confetti;
+    public int numEmit = 5000;
+
+	// Use this for initialization
+	void Start () {
+        confetti = GetComponentInChildren<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other) {
+        
+        if (other.tag == "Finish") {
+
+            confetti.Play();
+            //confetti.Emit(numEmit);
+            //var em = confetti.emission;
+            //em.enabled = true;
+
+            print("SCORE");
+        }
+        
+    }
+
+   
+}
